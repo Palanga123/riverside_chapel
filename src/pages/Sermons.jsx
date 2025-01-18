210caa1327fdec2c97ed86c9d906d1018fac0257
Palanga123 <palangageorgen@gmail.com>
2025-01-18 18:30:37 +0200

    added the sermons page

--- src/pages/Sermons.jsx
+++ src/pages/Sermons.jsx
@@ -1,6 +1,12 @@
 const Sermons = () => {
     return(
-        <p>Sermons</p>
+        <div className="h-80 w-full" id="hero">
+                <div className='w-full h-full' style={{ backgroundColor: 'rgba(0,0,0,0.3)' }}>
+                    <div className='text-white text-center py-32'>
+                        <p className='text-5xl uppercase font-extrabold'>Sermons</p>
+                    </div>
+                </div>
+            </div>
     )
 }
 
